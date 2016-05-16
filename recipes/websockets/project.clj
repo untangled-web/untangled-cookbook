ddde7c375e54aaa0f1c5ef496c51dad495bcc8ce
mahinshaw <mahinshaw@gmail.com>
2016-05-16 14:54:57 -0700

    Story ID: develop Update websockets deps.

--- recipes/websockets/project.clj
+++ recipes/websockets/project.clj
@@ -15,11 +15,10 @@
                  [com.cemerick/piggieback "0.2.1"]
                  [org.clojure/tools.nrepl "0.2.12"]
                  [juxt/dirwatch "0.2.3"]
-                 [navis/untangled-client "0.4.9-SNAPSHOT" :exclusions [cljsjs/react org.omcljs/om]]
-                 [navis/untangled-server "0.4.8-SNAPSHOT"]
-                 [navis/untangled-datomic "0.4.8" :exclusions [com.datomic/datomic-free org.clojure/tools.cli]]
+                 [navis/untangled-client "0.4.9" :exclusions [cljsjs/react org.omcljs/om]]
+                 [navis/untangled-server "0.4.7"]
                  [navis/untangled-spec "0.3.5"]
-                 [navis/untangled-websockets "0.1.0-SNAPSHOT"]]
+                 [navis/untangled-websockets "0.1.0"]]
 
   :plugins [[lein-cljsbuild "1.1.2"] [lein-environ "1.0.0"]]
 
