97cf418a6fcad66fd2f80f7637f2a2647aa5c052
Tony Kay <twkay@thenavisway.com>
2016-06-16 16:17:52 -0700

    added to-many union example

--- recipes/initial-app-state/src/client/app/ui.cljs
+++ recipes/initial-app-state/src/client/app/ui.cljs
@@ -32,6 +32,51 @@
 
 (def ui-child (om/factory Child {:keyfn :id}))
 
+(defui Foo
+  static InitialAppState
+  (initial-state [clz params] (merge {:type :foo} params))
+  static om/IQuery
+  (query [this] [:type :id :label])
+  Object
+  (render [this]
+    (let [{:keys [label]} (om/props this)]
+      (dom/div nil
+        (dom/h2 nil "Foo")
+        (dom/p nil label)))))
+
+(def ui-foo (om/factory Foo {:keyfn :id}))
+
+(defui Bar
+  static InitialAppState
+  (initial-state [clz params] (merge {:type :bar} params))
+  static om/IQuery
+  (query [this] [:type :id :label])
+  Object
+  (render [this]
+    (let [{:keys [label]} (om/props this)]
+      (dom/div nil
+        (dom/h2 nil "Bar")
+        (dom/p nil label)))))
+
+(def ui-bar (om/factory Bar {:keyfn :id}))
+
+(defui ListItem
+  static InitialAppState
+  (initial-state [clz params] [(initial-state Bar {:id 1 :label "A"}) (initial-state Foo {:id 2 :label "B"}) (initial-state Bar {:id 3 :label "C"})])
+  static om/IQuery
+  (query [this] {:foo (om/get-query Foo) :bar (om/get-query Bar)})
+  static om/Ident
+  (ident [this props] [(:type props) (:id props)])
+  Object
+  (render [this]
+    (let [{:keys [type] :as props} (om/props this)]
+      (case type
+        :foo (ui-foo props)
+        :bar (ui-bar props)
+        (dom/p nil "No Item renderer!")))))
+
+(def ui-list-item (om/factory ListItem {:keyfn :id}))
+
 (defui ^:once Settings
   static InitialAppState
   (initial-state [clz params] {:type :settings :id :singleton})
@@ -74,22 +119,26 @@
 (defui ^:once Root
   static InitialAppState
   (initial-state [clz params] {:ui/react-key "abc"
-                                  :children     []
-                                  :panes        (initial-state PaneSwitcher nil)})
+                               :children     []
+                               :panes        (initial-state PaneSwitcher nil)
+                               :items        (initial-state ListItem nil)})
   static om/IQuery
   (query [this] [:ui/react-key :value {:children (om/get-query Child)}
+                 {:items (om/get-query ListItem)}
                  {:panes (om/get-query PaneSwitcher)}])
   Object
   (render [this]
-    (let [{:keys [ui/react-key value children panes] :or {value ""}} (om/props this)]
+    (let [{:keys [ui/react-key value children panes items] :or {value ""}} (om/props this)]
       (dom/div #js {:key react-key}
         (dom/input #js {:type "text" :value value})
         (dom/button #js {:onClick (fn [evt] (om/transact! this '[(nav/settings)]))} "Go to settings")
         (dom/button #js {:onClick (fn [evt] (om/transact! this '[(nav/main)]))} "Go to main")
         (dom/button #js {:onClick (fn [evt] (om/transact! this '[(set-to-tony)]))} "Set to Tony")
         (ui-panes panes)
-        (mapv ui-child children)))))
-
+        (mapv ui-child children)
+        (dom/h1 nil "Heterogenous list:")
+        (dom/ul nil
+          (mapv ui-list-item items))))))
 
 (comment
   (uc/merge-state! @app.core/app Child {:id 1 :label {:id 41 :value "Blammo!"}} :append [:children])
