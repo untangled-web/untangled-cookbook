72c8c139486c1f8514b762025cb653014f857663
Tony Kay <twkay@thenavisway.com>
2016-08-24 15:31:36 -0700

    updated c.l.s. readme and added some more comments

--- recipes/component-local-state/README.md
+++ recipes/component-local-state/README.md
@@ -1,6 +1,6 @@
 # Component Local State
 
-This recipe demonstrates some common use-cases for component local state. In general you want to avoid component
+This recipe demonstrates a good use-case for component local state. In general you want to avoid component
 local state because it violates a basic useful property of Untangled components: That they are pure functions. If
 you locate all of your state in the central application database, then you get a variety of advantages, such as
 easier reasoning about the rendering, and support from the support viewer being able to show the component
@@ -13,10 +13,17 @@ However, in some cases it is reasonable to relax and use local state. The two mo
 - The cost of updating the central atom, running a database query, and re-rendering are too much overhead. An example
 of this is trying to render some kind of animation or interactive graphical interaction within a component.
 
+The code in this recipe demonstrates a square being positioned on an HTML5 canvas. In the demonstration there is a
+gray rectangle that follows the mouse when it is over the canvas. This hover behavior requires frequent updates,
+and would quickly overwrite all of the application state history within seconds. The tracking of the mouse
+is neither of interest to us (in a support sense), and would also pose a potential performance
+problem if done via top-level transactions.
+
+However, the size of the canvas and selected position of the square are things that might be of interest to us
+in the app state, and are therefore modelled that way. Thus, the overall example shows a mixture of
+client application database state mixed with component-local state.
+
 ## Running 
 
 This is a client-only example. Run the client as explained in the top-level cookbook README.
 
-## Form Field Details
-
-## Canvas Details
--- recipes/component-local-state/src/client/app/ui.cljs
+++ recipes/component-local-state/src/client/app/ui.cljs
@@ -37,23 +37,31 @@
         marker (:marker props)
         size (:size props)
         real-marker-coords (mapv (partial * size) marker)
-        ctx (.getContext canvas "2d")]
+        ; See HTML5 canvas docs
+        ctx (.getContext canvas "2d")
+        clear (fn []
+                (set! (.-fillStyle ctx) "white")
+                (.fillRect ctx 0 0 size size))
+        drawHover (fn []
+                    (set! (.-strokeStyle ctx) "gray")
+                    (.strokeRect ctx (- (first hover) 5) (- (second hover) 5) 10 10))
+        drawMarker (fn []
+                     (set! (.-strokeStyle ctx) "red")
+                     (.strokeRect ctx (- (first real-marker-coords) 5) (- (second real-marker-coords) 5) 10 10))]
     (.save ctx)
-    (set! (.-fillStyle ctx) "white")
-    (set! (.-strokeStyle ctx) "gray")
-    (.fillRect ctx 0 0 size size)
-    (.strokeRect ctx (- (first hover) 5) (- (second hover) 5) 10 10)
-    (set! (.-strokeStyle ctx) "red")
-    (.strokeRect ctx (- (first real-marker-coords) 5) (- (second real-marker-coords) 5) 10 10)
+    (clear)
+    (drawHover)
+    (drawMarker)
     (.restore ctx)))
 
 (defn place-marker
-  "Update the marker in app state. Derives normalized coordinates."
+  "Update the marker in app state. Derives normalized coordinates, and updates the marker in application state."
   [child evt]
   (om/transact! child `[(canvas/place-marker {:coords ~(event->normalized-coords evt (om/get-state child :canvas))})]))
 
 (defn hover-marker
-  "Updates the hover location of a proposed marker using canvas coordinates."
+  "Updates the hover location of a proposed marker using canvas coordinates. Hover location is stored in component
+  local state (meaning that a low-level app database query will not run to do the render that responds to this change)"
   [child evt]
   (om/update-state! child assoc :coords (event->dom-coords evt (om/get-state child :canvas)))
   (render-hover-and-marker (om/props child) (om/get-state child)))
@@ -67,14 +75,17 @@
   (ident [this props] [:child/by-id (:id props)])
   Object
   (initLocalState [this] {:coords [-50 -50]})
+  ; Remember that this "render" just renders the DOM (e.g. the canvas DOM element). The graphical rendering within the canvas is done during event handling.
   (render [this]
     (let [{:keys [size]} (om/props this)]
+      ; size comes from props. Transactions on size will cause the canvas to resize in the DOM
       (dom/canvas #js {:width       (str size "px")
                        :height      (str size "px")
                        :onMouseDown (fn [evt] (place-marker this evt))
                        :onMouseMove (fn [evt] (hover-marker this evt))
+                       ; This is a pure React mechanism for getting the underlying DOM element.
+                       ; Note: when the DOM element changes this fn gets called with nil (to help you manage memory leaks), then the new element
                        :ref         (fn [r]
-                                      ; when ref is updated, this fn gets called with nil (to help you manage memory leaks), then the new canvas
                                       (when r
                                         (om/update-state! this assoc :canvas r)
                                         (render-hover-and-marker (om/props this) (om/get-state this))))
