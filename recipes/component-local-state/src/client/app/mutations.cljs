017194636a5716a9cc4bcd772a93d5a33d3f3747
Tony Kay <twkay@thenavisway.com>
2016-08-24 14:42:44 -0700

    working version

--- recipes/component-local-state/src/client/app/mutations.cljs
+++ recipes/component-local-state/src/client/app/mutations.cljs
@@ -1,3 +1,16 @@
 (ns app.mutations
   (:require [untangled.client.mutations :as m]))
 
+(defn change-size [amount state]
+  (let [current-size (get-in @state [:child/by-id 0 :size])
+        new-size (+ amount current-size)]
+    (swap! state assoc-in [:child/by-id 0 :size] new-size)))
+
+(defmethod m/mutate 'canvas/make-smaller [{:keys [state]} k p] {:action (partial change-size -20 state)})
+
+(defmethod m/mutate 'canvas/make-bigger [{:keys [state]} k p] {:action (partial change-size 20 state)})
+
+(defmethod m/mutate 'canvas/place-marker [{:keys [state]} k {:keys [coords]}]
+  {:action (fn []
+             (swap! state assoc-in [:child/by-id 0 :marker] coords))})
+
--- recipes/component-local-state/src/client/app/ui.cljs
+++ recipes/component-local-state/src/client/app/ui.cljs
@@ -5,20 +5,79 @@
             [untangled.client.core :refer [InitialAppState initial-state]]
             yahoo.intl-messageformat-with-locales))
 
+(defn event->dom-coords
+  "Translate a javascript evt to a clj [x y] within the given dom element."
+  [evt dom-ele]
+  (let [cx (.-clientX evt)
+        cy (.-clientY evt)
+        BB (.getBoundingClientRect dom-ele)
+        x (- cx (.-left BB))
+        y (- cy (.-top BB))]
+    [x y]))
+
+(defn event->normalized-coords
+  "Translate a javascript evt to a clj [x y] within the given dom element as normalized (0 to 1) coordinates."
+  [evt dom-ele]
+  (let [cx (.-clientX evt)
+        cy (.-clientY evt)
+        BB (.getBoundingClientRect dom-ele)
+        w (- (.-right BB) (.-left BB))
+        h (- (.-bottom BB) (.-top BB))
+        x (/ (- cx (.-left BB))
+             w)
+        y (/ (- cy (.-top BB))
+             h)]
+    [x y]))
+
+(defn render-hover-and-marker
+  "Render the graphics in the canvas. Pass the component props and state. "
+  [props state]
+  (let [canvas (:canvas state)
+        hover (:coords state)
+        marker (:marker props)
+        size (:size props)
+        real-marker-coords (mapv (partial * size) marker)
+        ctx (.getContext canvas "2d")]
+    (.save ctx)
+    (set! (.-fillStyle ctx) "white")
+    (set! (.-strokeStyle ctx) "gray")
+    (.fillRect ctx 0 0 size size)
+    (.strokeRect ctx (- (first hover) 5) (- (second hover) 5) 10 10)
+    (set! (.-strokeStyle ctx) "red")
+    (.strokeRect ctx (- (first real-marker-coords) 5) (- (second real-marker-coords) 5) 10 10)
+    (.restore ctx)))
+
+(defn place-marker
+  "Update the marker in app state. Derives normalized coordinates."
+  [child evt]
+  (om/transact! child `[(canvas/place-marker {:coords ~(event->normalized-coords evt (om/get-state child :canvas))})]))
+
+(defn hover-marker
+  "Updates the hover location of a proposed marker using canvas coordinates."
+  [child evt]
+  (om/update-state! child assoc :coords (event->dom-coords evt (om/get-state child :canvas)))
+  (render-hover-and-marker (om/props child) (om/get-state child)))
+
 (defui ^:once Child
   static InitialAppState
-  (initial-state [cls _] {:id 0})
+  (initial-state [cls _] {:id 0 :size 50 :marker [0.5 0.5]})
   static om/IQuery
-  (query [this] [:id])
+  (query [this] [:id :size :marker])
   static om/Ident
   (ident [this props] [:child/by-id (:id props)])
   Object
-  (initLocalState [this] {})
-  (componentDidMount [this] (js/console.log :mount (om/props this)))
-  (componentWillReceiveProps [this props] (js/console.log :will props))
-  (componentWillUpdate [this next-props next-state] (js/console.log :will next-props :st next-state :oldprops (om/props this)))
+  (initLocalState [this] {:coords [-50 -50]})
   (render [this]
-    (dom/canvas #js {:width "100px" :height "100px" :style #js {:border "1px solid black"}})))
+    (let [{:keys [size]} (om/props this)]
+      (dom/canvas #js {:width       (str size "px")
+                       :height      (str size "px")
+                       :onMouseDown (fn [evt] (place-marker this evt))
+                       :onMouseMove (fn [evt] (hover-marker this evt))
+                       :ref         (fn [r]
+                                      (om/update-state! this assoc :canvas r)
+                                      (when r
+                                        (render-hover-and-marker (om/props this) (om/get-state this))))
+                       :style       #js {:border "1px solid black"}}))))
 
 (def ui-child (om/factory Child))
 
@@ -31,8 +90,10 @@
   Object
   (render [this]
     (let [{:keys [ui/react-key child]} (om/props this)]
-      (js/console.log react-key)
       (dom/div #js {:key react-key}
-        (dom/p nil "Ho")
+        (dom/button #js {:onClick #(om/transact! this '[(canvas/make-bigger)])} "Bigger!")
+        (dom/button #js {:onClick #(om/transact! this '[(canvas/make-smaller)])} "Smaller!")
+        (dom/br nil)
+        (dom/br nil)
         (ui-child child)))))
 
